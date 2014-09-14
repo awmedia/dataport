9bd2aa712a182957163c5dc94e27724bbf54b4b5
Jerry <email@sjerry.nl>
2014-09-14 22:02:14 +0200

    spaces for tabs

--- lib/Aw/DataPort/Processor.php
+++ lib/Aw/DataPort/Processor.php
@@ -11,69 +11,69 @@ use \Exception,
  */
 abstract class Processor
 {
-	/**
-	 * @cfg string  The method prefix for cell processor methods
-	 */
-	private $cellMethodPrefix = 'process_';
-	
-	public function __construct()
-	{
-    	$this->_processedRow;
-    	$this->_processorMethodsBuffer = array();
-	}
-	
-	/**
-	 * Process
-	 * @param   array
-	 * @param   array   
-	 * @param   array   Array with columns or null
-	 * @param   object  Mapper instance
-	 * @param   mixed   Processed result
-	 */
-	public function processRow(array $mappedRow, array $sourceRow, $sourceColumns = null, Mapper $mapper)
-	{
-    	$this->_processedRow = array();
-    	
-    	foreach ($mappedRow as $column => $cell)
-    	{
-        	$this->_processedRow[$column] = $this->processCell($column, $cell, $mappedRow, $sourceRow, $sourceColumns, $mapper);
-    	}
-    	
-    	return $this->_processedRow;
-	}
-	
-	/**
-	 * Private/protected methods
-	 */
-	
-	private function processCell($column, $cell, $mappedRow, $sourceRow, $sourceColumns, $mapper)
-	{
-    	if (!array_key_exists($column, $this->_processorMethodsBuffer))
-    	{
-        	$method = $this->cellMethodPrefix . str_replace(array(' ', '-', ':'), '_', $column);
-        	$this->_processorMethodsBuffer[$column] = method_exists($this, $method) ? $method : null;
-    	}
-    	else
-    	{
-        	$method = $this->_processorMethodsBuffer[$column];
-    	}
-    	
-    	if ($method)
-    	{
-        	$destinationColumnName = $mapper->getDestinationColumnForSource($column);
-        	$cell = $this->{$method}($cell, $mappedRow, $sourceRow, $sourceColumns, $destinationColumnName);
-    	}
-		
-		return $cell;
-	}
-	
-	protected function isCellProcessed($destinationColumn)
-	{
-    	return array_key_exists($destinationColumn, $this->_processedRow);
-	}
-	
-	protected function getConvertedCell($destinationColumn)
-	{
-		return $this->_isCellConverted($destinationColumn) ? $this->_processedRow[$destinationColumn] : null;
-	}
+    /**
+     * @cfg string  The method prefix for cell processor methods
+     */
+    private $cellMethodPrefix = 'process_';
+    
+    public function __construct()
+    {
+        $this->_processedRow;
+        $this->_processorMethodsBuffer = array();
+    }
+    
+    /**
+     * Process
+     * @param   array
+     * @param   array   
+     * @param   array   Array with columns or null
+     * @param   object  Mapper instance
+     * @param   mixed   Processed result
+     */
+    public function processRow(array $mappedRow, array $sourceRow, $sourceColumns = null, Mapper $mapper)
+    {
+        $this->_processedRow = array();
+        
+        foreach ($mappedRow as $column => $cell)
+        {
+            $this->_processedRow[$column] = $this->processCell($column, $cell, $mappedRow, $sourceRow, $sourceColumns, $mapper);
+        }
+        
+        return $this->_processedRow;
+    }
+    
+    /**
+     * Private/protected methods
+     */
+    
+    private function processCell($column, $cell, $mappedRow, $sourceRow, $sourceColumns, $mapper)
+    {
+        if (!array_key_exists($column, $this->_processorMethodsBuffer))
+        {
+            $method = $this->cellMethodPrefix . str_replace(array(' ', '-', ':'), '_', $column);
+            $this->_processorMethodsBuffer[$column] = method_exists($this, $method) ? $method : null;
+        }
+        else
+        {
+            $method = $this->_processorMethodsBuffer[$column];
+        }
+        
+        if ($method)
+        {
+            $destinationColumnName = $mapper->getDestinationColumnForSource($column);
+            $cell = $this->{$method}($cell, $mappedRow, $sourceRow, $sourceColumns, $destinationColumnName);
+        }
+        
+        return $cell;
+    }
+    
+    protected function isCellProcessed($destinationColumn)
+    {
+        return array_key_exists($destinationColumn, $this->_processedRow);
+    }
+    
+    protected function getConvertedCell($destinationColumn)
+    {
+        return $this->_isCellConverted($destinationColumn) ? $this->_processedRow[$destinationColumn] : null;
+    }
 }
--- lib/Aw/DataPort/Result.php
+++ lib/Aw/DataPort/Result.php
@@ -10,39 +10,39 @@ use \Exception;
  */
 class Result
 {
-	protected $success;
-	
-	protected $writerResult;
-	
-	protected $rowCount;
-	
-	protected $exception;
-	
-	public function __construct($success, $writerResult, $rowCount, Exception $exception = null)
-	{
-		$this->success = (bool) $success;
-		$this->writerResult = $writerResult;
-		$this->rowCount = $rowCount;
-		$this->exception = $exception;
-	}
-	
-	public function getSuccess()
-	{
-		return $this->success;
-	}
-	
-	public function getWriterResult()
-	{
-		return $this->writerResult;
-	}
-	
-	public function getRowCount()
-	{
-		return $this->rowCount;
-	}
-	
-	public function getException()
-	{
-		return $this->exception;
-	}
+    protected $success;
+    
+    protected $writerResult;
+    
+    protected $rowCount;
+    
+    protected $exception;
+    
+    public function __construct($success, $writerResult, $rowCount, Exception $exception = null)
+    {
+        $this->success = (bool) $success;
+        $this->writerResult = $writerResult;
+        $this->rowCount = $rowCount;
+        $this->exception = $exception;
+    }
+    
+    public function getSuccess()
+    {
+        return $this->success;
+    }
+    
+    public function getWriterResult()
+    {
+        return $this->writerResult;
+    }
+    
+    public function getRowCount()
+    {
+        return $this->rowCount;
+    }
+    
+    public function getException()
+    {
+        return $this->exception;
+    }
 }
