ce5ee2a4f1b105a09fbb7b2fdefd24e60388a498
Jerry <email@sjerry.nl>
2014-09-15 20:56:51 +0200

    Create Filter class

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ dataport
 ========
 
 Library for fast and abstract data conversion.
+
+
+
+Tmp docs:
+
+
+$dataPorter->getFilters()->add(Filter::factory('quantity', function($value) { return $value > 0; }));
+$dataPorter->getFilters()->add(Filter::factory('quantity', 'filter_var', array(':value', FILTER_VALIDATE_INT, array('options'=>array('min_range'=>0, 'max_range'=>100)))));
---
+++ lib/Aw/DataPort/Filter.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Aw\DataPort;
+
+use \callable,
+    Aw\Common\Utils\ArrayUtils as Arr;
+
+/**
+ * Filter
+ * @author Jerry Sietsma
+ */
+class Filter
+{
+    protected $columns;
+    
+    protected $callable;
+    
+    protected $params;
+    
+    protected $hasCustomParamSignature = false;
+    
+    protected $valuePlaceholderIndexes;
+    
+    /**
+     * Factory method
+     * Helper to create single-line Filter instances
+     * @info    The params will be forwarded to the constructor
+     */
+    public static function factory()
+	{
+		$class = new \ReflectionClass(get_called_class());
+		return $class->newInstanceArgs(func_get_args());
+	}
+    
+    /**
+     * @param   mixed       String with column or array with column names
+     * @param   callable    Filter callable. The filter callable should ta
+     * @param   array       Params for filter callable or null. 
+     */
+    public function __construct($column, callable $callable, $params = null)
+    {
+        $this->columns = is_array($column) ? $column : array($columns);
+        $this->callable = $callable;
+        $this->params = $params;
+        
+        if (is_array($params))
+        {
+            $result = array_filter($params, function($value) { return stristr($value, ':value'); } );
+            
+            if (count($result) > 0)
+            {
+                $this->valuePlaceHolderIndexes = array_keys($result);
+                $this->hasCustomParamSignature = true;
+            }
+        }
+    }
+    
+    /**
+     * Accept?
+     * When the filter returns false for one or more columns, the row will be ignored
+     * @param   array   Row
+     * @return  bool    True if accepted, false if not
+     */
+    public function accept($row)
+    {
+        $accept = 1;
+        
+        $params = $this->params;
+        
+        foreach ($this->columns as $column)
+        {
+            $value = Arr::get($row, $column);
+            
+            if (!$this->hasCustomParamSignature)
+            {
+                // prepend value as first param
+                array_unshift($params, $value);
+            }
+            else
+            {
+                // replace placeholders with value
+                $params = array_map(function())
+                foreach ($this->valuePlaceHolderIndexes as $index)
+                {
+                    $params[$index] = strtr($params[$index], array(':value'=>$value));
+                }
+            }
+            
+            $accept &= call_user_func_array($this->callable, $params) === true;
+        }
+        
+        return (bool) $accept;
+    }
+}
